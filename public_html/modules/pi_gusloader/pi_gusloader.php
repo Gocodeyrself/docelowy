910ef240fb425dc52fd2bfb1e51fd61ba4c75070
Szymon Szewczyk <s.szewczyk@cezos.com>
2024-12-04 14:07:49 +0100

    przywrocenie gusloadera

--- public_html/modules/pi_gusloader/pi_gusloader.php
+++ public_html/modules/pi_gusloader/pi_gusloader.php
@@ -21,16 +21,18 @@ class Pi_gusloader extends Module
         parent::__construct();
 
         $this->displayName = $this->l('Pobieranie danych z GUSu');
-        $this->description = $this->l('Pobiera dane do firmy z bazy GUS w trakcie składania zamówienia');
+        $this->description = $this->l('Pobiera dane do firmy z bazy GUS w trakcie składanie zamówienia');
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+        $this->registerHook('displayShoppingCartFooter');
     }
 
     public function install()
     {
         return parent::install() && $this->registerHook('actionFrontControllerSetMedia')
             && $this->registerHook('displayCheckoutSummaryTop')
-            && $this->registerHook('displayGus');
+            && $this->registerHook('displayGus')
+            && $this->registerHook('displayShoppingCartFooter');
     }
 
     public function uninstall()
@@ -46,13 +48,15 @@ class Pi_gusloader extends Module
         $this->context->controller->addJS($this->_path . 'pi_gusloader.js');
         $this->context->controller->addCSS($this->_path . 'pi_gusloader.css');
     }
-
     public function hookDisplayCheckoutSummaryTop()
     {
         return $this->display(__FILE__, 'pi_gusloader.tpl');
     }
-
     public function hookDisplayGus()
+    {
+        return $this->display(__FILE__, 'pi_gusloader.tpl');
+    }
+    public function hookDisplayShoppingCartFooter()
     {
         return $this->display(__FILE__, 'pi_gusloader.tpl');
     }
